848c96d7a2755706cc5782a23ad6667f32361482
Brayan Z <brayan@pernix-solutions.com>
2014-08-31 16:41:29 -0600

    adopt a second puppy

--- test_puppies/features/step_definitions/adopting_puppy_steps.rb
+++ test_puppies/features/step_definitions/adopting_puppy_steps.rb
@@ -2,8 +2,12 @@ Given(/^I am on the puppy adoption site$/) do
   @browser.goto 'http://puppies.herokuapp.com'
 end
 
-When(/^I click the View Details button$/) do
-  @browser.button(value: 'View Details').click
+When(/^I click the first View Details button$/) do
+  @browser.button(value: 'View Details', index: 0).click
+end
+
+When(/^I click the second View Details button$/) do
+  @browser.button(value: 'View Details', index: 1).click
 end
 
 When(/^I click the Adopt Me button$/) do
@@ -13,6 +17,11 @@ end
 When(/^I click the Complete the Adoption button$/) do
   @browser.button(value: 'Complete the Adoption').click
 end
+
+When(/^I click the Adopt Another Puppy button$/) do
+  @browser.button(value: 'Adopt Another Puppy').click
+end
+
 
 When(/^I enter "(.*?)" in the name field$/) do |name|
   @browser.text_field(id: 'order_name').set(name)
