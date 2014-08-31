733b26e6e9912a2473542461874638c7d00e5940
Brayan Z <brayan@pernix-solutions.com>
2014-08-31 15:32:54 -0600

    extract methods

--- first_script.rb
+++ first_script.rb
@@ -1,24 +1,41 @@
 require 'rubygems'
 require 'watir-webdriver'
-@browser = Watir::Browser.new :firefox
+
+def go_to_the_adoption_site
+  @browser = Watir::Browser.new :firefox
+  @browser.goto 'http://puppies.herokuapp.com'
+end
 
 def adopt_puppy_number(number)
   @browser.button(value: 'View Details', index: number).click
   @browser.button(value: 'Adopt Me!').click
 end
 
-@browser.goto 'http://puppies.herokuapp.com'
+def adopt_another_puppy
+  @browser.button(value: 'Adopt Another Puppy').click
+end
+
+def checkout_with(name, address, email, pay_type)
+  @browser.button(value: 'Complete the Adoption').click
+  @browser.text_field(id: 'order_name').set(name)
+  @browser.text_field(id: 'order_address').set(address)
+  @browser.text_field(id: 'order_email').set(email)
+  @browser.select_list(id: 'order_pay_type').select(pay_type)
+  @browser.button(value: 'Place Order').click
+end
+
+def verify_page_contains(message)
+  fail 'Browser text did not match expected value' unless @browser.text.include? message
+end
+
+def close_browser
+  @browser.close
+end
+
+go_to_the_adoption_site
 adopt_puppy_number 0
-@browser.button(value: 'Adopt Another Puppy').click
+adopt_another_puppy
 adopt_puppy_number 1
-@browser.button(value: 'Complete the Adoption').click
-@browser.text_field(id: 'order_name').set('Cheezy')
-@browser.text_field(id: 'order_address').set('123 Main St.')
-@browser.text_field(id: 'order_email').set('cheezy@foo.com')
-@browser.select_list(id: 'order_pay_type').select('Check')
-@browser.button(value: 'Place Order').click
-fail 'Browser text did not match expected value' unless @browser.text.include? 'Thank you for adopting a puppy!'
-
-sleep 5
-@browser.close
-
+checkout_with('Cheezy', '123 Main St.', 'cheezy@foo.com', 'Check')
+verify_page_contains 'Thank you for adopting a puppy!'
+close_browser
