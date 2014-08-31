673b1d410424ff551b5b01e410849539369c39cf
Brayan Z <brayan@pernix-solutions.com>
2014-08-31 17:58:02 -0600

    use visit and on methods

--- test_puppies/features/step_definitions/adopting_puppy_steps.rb
+++ test_puppies/features/step_definitions/adopting_puppy_steps.rb
@@ -1,50 +1,45 @@
 Given(/^I am on the puppy adoption site$/) do
-  @browser.goto 'http://puppies.herokuapp.com'
-  @home = HomePage.new(@browser)
+  visit(HomePage)
 end
 
 When(/^I click the first View Details button$/) do
-  @home.select_puppy_number(1)
-  @details = DetailsPage.new(@browser)
+  on(HomePage).select_puppy_number(1)
 end
 
 When(/^I click the second View Details button$/) do
-  @home.select_puppy_number(2)
-  @details = DetailsPage.new(@browser)
+  on(HomePage).select_puppy_number(2)
 end
 
 When(/^I click the Adopt Me button$/) do
-  @details.add_to_cart
-  @shopping_cart = ShoppingCartPage.new(@browser)
+  on(DetailsPage).add_to_cart
 end
 
 When(/^I click the Complete the Adoption button$/) do
-  @shopping_cart.proceed_to_checkout
-  @checkout = CheckoutPage.new(@browser)
+  on(ShoppingCartPage).proceed_to_checkout
 end
 
 When(/^I click the Adopt Another Puppy button$/) do
-  @shopping_cart.continue_shopping
+  on(ShoppingCartPage).continue_shopping
 end
 
 When(/^I enter "(.*?)" in the name field$/) do |name|
-  @checkout.name = name
+  on(CheckoutPage).name = name
 end
 
 When(/^I enter "(.*?)" in the address field$/) do |address|
-  @checkout.address = address
+  on(CheckoutPage).address = address
 end
 
 When(/^I enter "(.*?)" in the email field$/) do |email|
-  @checkout.email = email
+  on(CheckoutPage).email = email
 end
 
 When(/^I select "(.*?)" from the pay with dropdown$/) do |pay_type|
-  @checkout.pay_type = pay_type
+  on(CheckoutPage).pay_type = pay_type
 end
 
 When(/^I click the Place Order button$/) do
-  @checkout.place_order
+  on(CheckoutPage).place_order
 end
 
 Then(/^I should see "(.*?)"$/) do |message|
@@ -52,13 +47,13 @@ Then(/^I should see "(.*?)"$/) do |message|
 end
 
 Then(/^I should see "(.*?)" as the name for line item (\d+)$/) do |name, line_item|
-  expect(@shopping_cart.name_for_line_item(line_item.to_i)).to include(name)
+  expect(on(ShoppingCartPage).name_for_line_item(line_item.to_i)).to include(name)
 end
 
 Then(/^I should see "(.*?)" as the subtotal for line item (\d+)$/) do |subtotal, line_item|
-  expect(@shopping_cart.subtotal_for_line_item(line_item.to_i)).to include(subtotal)
+  expect(on(ShoppingCartPage).subtotal_for_line_item(line_item.to_i)).to include(subtotal)
 end
 
 Then(/^I should see "(.*?)" as the cart total$/) do |total|
-  expect(@shopping_cart.cart_total).to eq(total)
+  expect(on(ShoppingCartPage).cart_total).to eq(total)
 end
--- test_puppies/features/support/pages/home_page.rb
+++ test_puppies/features/support/pages/home_page.rb
@@ -1,6 +1,8 @@
 class HomePage
   include PageObject
 
+  page_url('http://puppies.herokuapp.com')
+
   def select_puppy_number(number)
     button_element(value: 'View Details', index: number-1).click
   end
