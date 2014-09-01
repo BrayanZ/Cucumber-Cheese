99fdd6aa3843ee34cbe256f5d687babd11918cbb
Brayan Z <brayan@pernix-solutions.com>
2014-08-31 18:43:16 -0600

    use a hash of default data

--- test_puppies/features/step_definitions/adopting_puppy_steps.rb
+++ test_puppies/features/step_definitions/adopting_puppy_steps.rb
@@ -2,6 +2,21 @@ Given(/^I am on the puppy adoption site$/) do
   visit(HomePage)
 end
 
+When(/^I complete the adoption of a puppy$/) do
+  on(HomePage).select_puppy
+  on(DetailsPage).add_to_cart
+  on(ShoppingCartPage).proceed_to_checkout
+  on(CheckoutPage).checkout
+end
+
+When(/^I complete the adoption using a Credit card$/) do
+  on(CheckoutPage).checkout('pay_type' => 'Credit card')
+end
+
+When(/^I complete the adoption$/) do
+  on(CheckoutPage).checkout
+end
+
 When(/^I click the View Details button for "(.*?)"$/) do |name|
   on(HomePage).select_puppy(name)
 end
@@ -19,7 +34,7 @@ When(/^I click the Adopt Another Puppy button$/) do
 end
 
 When(/^I complete the adoption with:$/) do |table|
-  on(CheckoutPage).checkout_with(table.hashes.first)
+  on(CheckoutPage).checkout(table.hashes.first)
 end
 
 Then(/^I should see "(.*?)"$/) do |message|
--- test_puppies/features/support/pages/checkout_page.rb
+++ test_puppies/features/support/pages/checkout_page.rb
@@ -1,13 +1,21 @@
 class CheckoutPage
   include PageObject
 
+  DEFAULT_DATA = {
+    'name' => 'Cheezy',
+    'address' => '123 Main St.',
+    'email' => 'cheezy@foo.com',
+    'pay_type' => 'Purchase order'
+  }
+
   text_field(:name, id: 'order_name')
   text_field(:address, id: 'order_address')
   text_field(:email, id: 'order_email')
   select_list(:pay_type, id: 'order_pay_type')
   button(:place_order, value: 'Place Order')
 
-  def checkout_with(data)
+  def checkout(data = {})
+    data = DEFAULT_DATA.merge(data)
     self.name = data['name']
     self.address = data['address']
     self.email = data['email']
